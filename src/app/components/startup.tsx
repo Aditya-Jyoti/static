99c66aff602c11786edd2e3bd5c9b811173dffac
AltSumpreme <reubenphilip52009@gmail.com>
2025-01-08 22:38:01 +0530

    removed the padding and gradient --reuben

--- src/app/components/startup.tsx
+++ src/app/components/startup.tsx
@@ -14,9 +14,9 @@ const Startup = () => {
   const sections = ['Section 1', 'Section 2', 'Section 3', 'Section 4', 'Section 5'];
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-6 sm:px-12 md:px-24 bg-gradient-to-r from-[#3a2c5d] via-[#4d3c74] to-[#1c2129]">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-center mb-20 text-[#B796FE]" 
+        <h1 className="text-4xl font-bold text-center mb-18 text-white" 
             style={{ 
               fontFamily: 'Instrument Sans, Arial, sans-serif', 
               letterSpacing: '0.1em' 
@@ -29,8 +29,8 @@ const Startup = () => {
 
       {/* Sections with grids */}
       {sections.map((section, index) => (
-        <div key={index} className="w-full max-w-4xl mb-10 lg:hidden text-center">
-          <h2 className="text-3xl font-semibold text-[#B796FE] mb-6">{section}</h2>
+        <div key={index} className="w-screen min-h-screen max-w-4xl mb-10 lg:hidden text-center">
+          <h2 className="text-3xl font-semibold text-white mb-6">{section}</h2>
 
           {/* Grid layout for smaller screens */}
           <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:hidden">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,6 @@ export default function Home() {
         <Teampage/>
         <Startup/>
       </section>
-
     </section>
 
   );
